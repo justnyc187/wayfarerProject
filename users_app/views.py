4370e2e10ddbefa3b414c5b605bd2b378cce54ed
jonathancode <parkj97@gmail.com>
2021-05-14 12:35:39 -0700

    made submaster

---
+++ users_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages 
+
+
+def register(request):
+    if request.method == "POST":
+        register_form = UserCreationForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request, ("New User Account Created, Please Log In"))
+            return redirect('register')
+    else:
+        register_form = UserCreationForm() 
+    return render(request, 'register.html', {'register_form':register_form})
+
