13c2ba5ed1516f41761f9728b970f9a42a745809
jonathancode <parkj97@gmail.com>
2021-05-16 11:52:12 -0700

    removed test url in appfolder

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('accounts/signup', views.SignUp.as_view(), name="signup"),
-    path('test/', views.Test.as_view(), name="test"),
 ]
 
