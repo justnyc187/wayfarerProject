fae0a5bd793f8509229808e3b1f6f458651fa16f
jonathancode <parkj97@gmail.com>
2021-05-17 15:06:43 -0700

    commit

--- db/models.sql
+++ db/models.sql
@@ -3,4 +3,8 @@ INSERT INTO main_app_cities(name, img, content)
     ('New York', 'main_app/static/styles/images/newyork.jpg', 'The Big Apple'),
     ('Jersey City', 'main_app/static/styles/images/newjersey.jpg', 'Everythings bigger in Texas'),
     ('Los Angeles', 'main_app/static/images/california.jpg', 'Golden gate bridge');
+
 SELECT * FROM main_app_cities;
+
+
+-- DROP TABLE main_app_cities; 
