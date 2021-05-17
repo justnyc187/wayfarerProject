b737c359f1af7c5868969932c29f8a741222bdc9
jonathancode <parkj97@gmail.com>
2021-05-17 14:33:01 -0700

    fixes search

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('accounts/signup', views.SignUp.as_view(), name="signup"),
+    path('profile/', views.Profile.as_view(), name="profile"),
 ]
 
 
