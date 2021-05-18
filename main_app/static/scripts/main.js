61f9058fb94592e31991e8902a8d8a8fd7d77453
justnyc <jcairo187@gmail.com>
2021-05-18 09:29:52 -0400

    fixed images

--- main_app/static/scripts/main.js
+++ main_app/static/scripts/main.js
@@ -1,8 +1,1 @@
-$(".auth_form").click(function (event) {
-    $(this).toggleClass("auth_form-trigger");
-  });
 
-
-// //   $(".auth_form").click(function (event) {
-// //     $(this).toggleClass("auth_form");
-// //   });
